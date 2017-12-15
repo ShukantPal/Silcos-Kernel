04842b23ed8e4dcce6e76dca0996ac5394c0ed37
Sukant Pal <SukantK3.4@gmail.com>
2017-12-15 17:10:57 +0530

    Changed coding style
    Change-Id: I99de4b2257a50f73f7a44534e29dbe3e0603a0b0

---
+++ Interface/Module/Elf/ABI/__cxa_pure_virtual.h
@@ -0,0 +1,10 @@
+#ifndef INTERFACE_MODULE_ELF_ABI___CXA_PURE_VIRTUAL_H_
+#define INTERFACE_MODULE_ELF_ABI___CXA_PURE_VIRTUAL_H_
+
+extern "C" void __cxa_pure_virtual()
+{
+
+}
+
+
+#endif/* Module/Elf/ABI/__cxa_pure_virtual.h */
